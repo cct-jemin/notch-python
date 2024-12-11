54c2568d98cf382c2a7ecd0e1123b4225886fd82
Jemin Shah <Jemin.s@cctdigisol.local>
2024-12-11 13:05:20 +0530

    remove task add log

--- app/routers/massupload.py
+++ app/routers/massupload.py
@@ -3,11 +3,14 @@ from app.config import massupload_config
 import os
 import logging
 from app.helpers import massupload_utils
+import time
 
 router = APIRouter()
 
 @router.post("/v2/carbon_data/sheet_wise_mass_upload")
 async def sheet_wise_mass_upload(file:UploadFile = File(...)):
+    start_time = time.time()
+    print(f"Task start time {start_time:.2f}")
     # validate upload file extension
     if not any(file.filename.endswith(ext) for ext in massupload_config.EXTENSION):
         message = "Unsupported file type.upload only xlsx file"
@@ -30,6 +33,7 @@ async def sheet_wise_mass_upload(file:UploadFile = File(...)):
           
         response = await massupload_utils.sheetWiseValidation(filePath)
         
+        print(f"Finished task:{time.time():.2f}, duration: {time.time() - start_time:.2f} seconds")
         if response['isAllSheetValid'] :
             return {"message":"file uploaded successfully","sheetdata":response['sheetWiseData']}
         else :
